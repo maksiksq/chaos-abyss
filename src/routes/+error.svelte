e47df6903f0e5b0af4a25b57f9de786a0564f272
Maksiks <maksiksking@gmail.com>
2025-07-02 13:51:14 +0300

    fix: added a fish

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -1,1 +1,24 @@
-<h1>oh no i'm an error</h1>
+<script>
+    import '../global.css';
+</script>
+
+<main>
+    <h1>404</h1>
+    <small>Oh noie, something went very wrong</small>
+</main>
+
+<style>
+    main {
+        width: 100vw;
+        height: 100vh;
+
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+
+        background-image: url("/img/chaos404.png");
+        background-repeat: no-repeat;
+        background-size: cover;
+    }
+</style>
