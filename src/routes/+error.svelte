1393bae0d114cee052e191f9c6362317ce8f81f8
Maksiks <maksiksking@gmail.com>
2025-07-03 15:22:36 +0300

    fix: 404 now responsive + overengineered it for what most people won't even see lol

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -4,6 +4,7 @@
 </script>
 
 <main>
+    <div class="bg"></div>
     <h1>{page.status}</h1>
     <small>{page.error?.message}</small>
 </main>
@@ -12,14 +13,53 @@
     main {
         width: 100vw;
         height: 100vh;
-
         display: flex;
         flex-direction: column;
         justify-content: center;
         align-items: center;
 
-        background-image: url("/img/chaos404.png");
-        background-repeat: no-repeat;
-        background-size: cover;
+        & .bg {
+            position: absolute;
+            inset: 0;
+            z-index: 0;
+
+            width: 100vw;
+            height: 100vh;
+
+            background-image: url("/img/chaos404.webp");
+            background-repeat: no-repeat;
+            background-size: cover;
+            background-position: center;
+        }
+    }
+
+    @media (max-width: 768px) {
+        main {
+            align-items: flex-start;
+
+            & .bg {
+                background-image: url("/img/chaos404-mobile.webp");
+            }
+
+            & h1 {
+                margin-top: -10vh;
+            }
+
+            & small {
+                width: 150px;
+                word-break: break-word;
+            }
+
+            & h1, small {
+                position: relative;
+                margin-left: 10vw;
+                white-space: normal;
+                z-index: 1;
+                color: white;
+                mix-blend-mode: difference;
+            }
+        }
+
+
     }
 </style>
