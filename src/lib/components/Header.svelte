b66be832a58dcf72563424f1d2764bd78f0d8dd0
Maksiks <maksiksking@gmail.com>
2025-07-06 14:40:58 +0300

    style: 4th wall break x3

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -1,4 +1,5 @@
 <script>
+    let home = $state(true);
 </script>
 
 <header>
@@ -7,6 +8,17 @@
             <img src="/img/logo.png" alt="logo">
         </a>
         <input class="search" type="text" placeholder="Search..."/>
+        {#if home}
+            <svg class="fourth-wall" width="349" height="161" viewBox="0 0 349 161" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <rect x="4" y="7" width="341" height="154" fill="white"/>
+                <line x1="0.65184" y1="27.2778" x2="36.1279" y2="40.8958" stroke="black"/>
+                <line x1="53.6541" y1="104.377" x2="66.8656" y2="93.6786" stroke="black"/>
+                <line x1="237.336" y1="0.918389" x2="257.644" y2="11.7162" stroke="black"/>
+                <line x1="142.375" y1="54.6877" x2="105.911" y2="160.586" stroke="black"/>
+                <line y1="-0.5" x2="57" y2="-0.5" transform="matrix(-0.642788 0.766044 0.766044 0.642788 348.641 38)" stroke="black"/>
+            </svg>
+
+        {/if}
         <a class="about" href="https://www.youtube.com/watch?v=dQw4w9WgXcQ">
             About
         </a>
@@ -55,8 +67,13 @@
                 font-size: 1.1rem;
             }
 
-            & .about {
+            & .fourth-wall {
+                align-self: flex-start;
                 margin-left: auto;
+            }
+
+            & .about {
+                padding-left: 1.3rem;
                 padding-right: 3vw;
             }
         }
