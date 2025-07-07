a43aa79bef157ca3a33d59dc1607ea605532d048
Maksiks <maksiksking@gmail.com>
2025-07-07 23:04:48 +0300

    chore: minor cleanup

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -78,7 +78,6 @@
 
 <style>
     @import url('https://fonts.googleapis.com/css2?family=Roboto+Slab:wght@100..900&display=swap');
-    @import url('https://fonts.googleapis.com/css2?family=Proza+Libre:ital,wght@0,400;0,500;0,600;0,700;0,800;1,400;1,500;1,600;1,700;1,800&display=swap');
 
     :global {
         body {
@@ -106,8 +105,6 @@
                 line-height: 1.8;
 
                 & span {
-                    /*idk which*/
-                    /*font-family: 'Proza Libre', sans-serif;*/
                     font-family: 'Roboto Slab', sans-serif;
                     font-weight: normal;
                     padding-left: 2rem;
--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -4,5 +4,30 @@
 </script>
 
 <Header />
-
+<main>
+    <div class="wrap">
+        <h1>Who is behind all this</h1>
+    </div>
+</main>
 <Footer />
+
+<style>
+    :global {
+        html, body {
+            overflow-y: visible;
+        }
+    }
+
+    main {
+        & .wrap {
+            max-width: 1000px;
+            width: 100%;
+            padding-top: clamp(0.5rem, 1.2vw, 3rem);
+            margin: 0 auto;
+
+            & h1 {
+                font-family: 'Roboto Slab', sans-serif;
+            }
+        }
+    }
+</style>
