2c6fb1f734eff707683579a9eddaeb3006b8c26d
Maksiks <maksiksking@gmail.com>
2025-07-08 13:36:49 +0300

    chore: contact progress done

---
+++ src/lib/components/Socials.svelte
@@ -0,0 +1,58 @@
+<script lang="ts">
+    const {socials, inline = false} = $props();
+</script>
+
+{#if !inline}
+    <li class="heading">
+        Socials
+    </li>
+{/if}
+
+{#each socials as social}
+    <li class={`social so-${social} ${inline ? 'inline' : ''}`}>
+        <!--!Font Awesome Free 6.7.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2025 Fonticons, Inc.-->
+        <a href={social.link}>
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+                <path d={social.icon}/>
+            </svg>
+            {social.text}
+        </a>
+    </li>
+{/each}
+
+<style>
+    .inline {
+        padding-bottom: 1rem;
+
+        &:hover {
+            color: black;
+
+            transition: all 0.1s;
+            transform: scale(1.003);
+        }
+    }
+
+    .inline:nth-child(1) {
+        margin-top: 1rem;
+    }
+
+    .social {
+        display: grid;
+        flex-direction: row;
+        align-items: center;
+        cursor: pointer;
+
+        & a {
+            display: flex;
+            flex-direction: row;
+            align-items: center;
+
+            & svg {
+                width: 1.2rem;
+                height: 1.2rem;
+                padding-right: 0.5rem;
+                fill: currentColor;
+            }
+        }
+    }
+</style>
