809e0c3bd190baa0eee680f3a191e4e703bb967d
Maksiks <maksiksking@gmail.com>
2025-07-08 12:30:39 +0300

    feat: tooltip

--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -1,20 +1,64 @@
-<script>
+<script lang="ts">
     import Header from "$lib/components/Header.svelte";
     import Footer from "$lib/components/Footer/Footer.svelte";
+
+    let copy = $state('Click to copy');
+    let hovering = $state(false);
+
+    const handleTooltip = () => {
+        hovering = true;
+        copy = 'Click to copy';
+    }
+
+    const handleCopyClick = () => {
+        navigator.clipboard.writeText("maksiks.touch@gmail.com");
+
+        copy = 'Copied!';
+    }
+
+    const handleCopyKeyboard = (e: KeyboardEvent) => {
+        if (e.key === 'Enter' || e.key === ' ') {
+            e.preventDefault;
+            navigator.clipboard.writeText("maksiks.touch@gmail.com")
+        }
+
+        copy = 'Copied!';
+    }
+
+    const handleCopyOut = () => {
+        copy = 'Click to copy';
+        hovering = false;
+    }
+
+    let tooltip: HTMLElement | undefined = $state();
+
+    const handleCopyMove = (e: MouseEvent) => {
+        if (!tooltip) return;
+        tooltip.style.left = `${e.clientX+10}px`;
+        tooltip.style.top = `${e.clientY-20}px`;
+    }
 </script>
 
-<Header />
+<Header/>
 <main>
     <div class="wrap">
-        <h1>Who is behind all this</h1>
+        <h1>Contact me:</h1>
         <img class="pfp" src="/img/pfp40likesmth.png" alt="Maksiks profile pic">
         <div class="content">
-            <p>Muhahaha - I'm Maksiks, an incredibly tired wizard individual. <br><br>I live in Ukraine <img src="/img/flag-ua.png" alt="Ukrainian flag"> and it's fun, plus we have pierogi here, they're really good. I recommend you visit here someday.<br><br> My main thing is web development, but I do have a lot of interests and passions, a lot more than I should: games, japanese, designing things, drawing, music, translating things, anime, 3d, pixel art, the list goes on. <br><br> I currently study in a college, academically, I'm the best student in the whole thing. Although I kinda hate it, to be honest. I've always been a "gifted kid" and the idea never stuck off. I'm not some kinda big shot despite the apparent confidence. And no one is, I can tell from up here. <br><br> From a young age I learned English, best decision in my life. I now crawl the internet in search of information to process and store in unhealthy amounts on a daily basis. Although I unfortunately touch plenty of grass, and also I exercise.<br><br> <strong>Some things I like: </strong> Minecraft, Konosuba, VA-11 Hall-A, Frieren, Omori, Oneshot, Hades, Vocaloid music, Dead Cells, EDM music <br> <strong>Some things that keep me alive:</strong> caffeine, Svelte, Stardew Valley, badass Ukrainian soldiers on the frontlines, insanity
-            </p>
+            The best way to contact me is via email:<br>
+            <div class="email" onmouseenter={handleTooltip} onmousemove={handleCopyMove} onclick={handleCopyClick} onkeydown={handleCopyKeyboard} onmouseout={handleCopyOut}
+                 onblur={handleCopyOut} aria-label="email" tabindex="0" role="button">
+                {#if hovering}
+                    <div class="tooltip" bind:this={tooltip}>
+                        {copy}
+                    </div>
+                {/if}
+                maksiks.touch@gmail.com
+            </div>
         </div>
     </div>
 </main>
-<Footer contact={true} />
+<Footer contact={true}/>
 
 <style>
     @import url('https://fonts.googleapis.com/css2?family=Roboto+Slab:wght@100..900&display=swap');
@@ -25,8 +69,21 @@
         }
     }
 
+    .tooltip {
+        position: absolute;
+        pointer-events: none;
+        transition: none;
+        background: #666666; color: white;
+        padding: 1px 0.7rem;
+        border-radius: 1px;
+        font-family: "Anonymous Pro", sans-serif;
+        border:  1px solid black;
+        box-shadow: rgba(0, 0, 0, 0.15) 1.95px 1.95px 2.6px;
+    }
+
     main {
         padding-bottom: 10vh;
+
         & .wrap {
             display: grid;
             grid-template-columns: 1fr 30%;
@@ -45,6 +102,10 @@
             & .content {
                 margin-top: 1rem;
                 font-size: 1rem;
+
+                & .email {
+                    cursor: pointer;
+                }
 
                 & img {
                     display: inline;
