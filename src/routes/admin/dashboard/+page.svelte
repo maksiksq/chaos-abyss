b191b16795c37104d43b9cd0139b9363f8a6acca
Maksiks <maksiksking@gmail.com>
2025-07-22 18:12:04 +0300

    fix: made edit/publish fancy

---
+++ src/routes/admin/dashboard/+page.svelte
@@ -0,0 +1,12 @@
+<script>
+</script>
+
+<main>
+    <p>I mean. Nothing to dashboard here yet. It's all up there.</p>
+</main>
+
+<style>
+    main {
+        padding: 3rem;
+    }
+</style>
--- src/routes/admin/dashboard/list/+page.svelte
+++ src/routes/admin/dashboard/list/+page.svelte
@@ -15,12 +15,80 @@
 
 <main>
     <h1>At your service!</h1>
-    <div>
+    <div class="articles">
         {#each data.articles as article}
             <div class="article">
-                <p>{article.title}</p>
-                <button class="edit" onclick={() => {handleEdit(article)}}>Edit</button>
+                <div class="title">
+                    <p>{article.title}</p>
+                </div>
+                <div class="blurb">
+                    <p>{article.blurb}</p>
+                </div>
+                <div class="info">
+                    <p>{article.category}</p>
+                </div>
+                <div class="buttons">
+                    <button class="edit" onclick={() => {handleEdit(article)}}>Edit</button>
+                    <button class="visit"
+                            onclick={() => {goto(`/articles/${article.category.toLowerCase()}/${article.slug}`)}}>
+                        Visit
+                    </button>
+                </div>
             </div>
         {/each}
     </div>
 </main>
+
+<style>
+
+    main {
+        margin-top: 1rem;
+        margin-left: 1.4rem;
+
+        & .articles {
+            & .article {
+                display: flex;
+                flex-direction: column;
+
+                margin-top: 0.9rem;
+                padding-bottom: 0.5rem;
+
+                & div {
+                    display: flex;
+                    flex-direction: row;
+                }
+
+                & .title {
+                    font-size: 1.2rem;
+
+                    & p {
+                        border-bottom: 1px solid black;
+                    }
+                }
+
+                & .blurb {
+                    margin-top: 0.4rem;
+                }
+
+                & .info {
+                    margin-top: 0.4rem;
+                }
+
+                & .buttons {
+                    margin-top: 0.9rem;
+
+                    & button {
+                        all: unset;
+                        background-color: rgba(126, 0, 189, 0.4);
+                        border: 1px solid black;
+                        padding: 0.3rem 2rem;
+                        margin-right: 1rem;
+                        font-size: 0.9rem;
+                        cursor: pointer;
+                    }
+                }
+            }
+        }
+    }
+
+</style>
