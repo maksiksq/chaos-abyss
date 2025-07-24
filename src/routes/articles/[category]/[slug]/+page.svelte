00fb47d8dac82d39361561f8c93541d020148dce
Maksiks <maksiksking@gmail.com>
2025-07-24 20:45:54 +0300

    chore: oop'd metablock logic

--- src/routes/articles/[category]/[slug]/+page.svelte
+++ src/routes/articles/[category]/[slug]/+page.svelte
@@ -2,7 +2,7 @@
     import MarkdownBlock from '$lib/components/MarkdownBlock.svelte';
     import ArticleEndblock from "./ArticleEndblock.svelte";
     import {tick} from "svelte";
-    import {timestamptzToHumanDate} from "$lib/utils/timestamptzToHumanDate";
+    import MetaBlock from "$lib/components/MetaBlock.svelte";
 
     const { data } = $props();
 
@@ -33,28 +33,15 @@
                     </figure>
                 </div>
                 <section class="content">
-                    <header class="metablock">
-                        <h1>
-                            <span>{data.article.title}</span>
-                        </h1>
-
-                        <small class="blurb"><span>{data.article.blurb}</span></small>
-                        <footer>
-                            <div class="databloc">
-                                <time datetime={timestamptzToHumanDate(data.article.date)}>{timestamptzToHumanDate(data.article.date)} {data.article.last_edit ? `| upd. ${timestamptzToHumanDate(data.article.last_edit)}` : ''} ✦&nbsp;</time>
-                                <address>
-                                    <p>
-                                        {data.wordcount}
-                                        words written by
-                                    </p>
-                                    <a rel="author" href={data.authorlink}>
-                                        {data.article.author}
-                                    </a>
-                                </address>
-                            </div>
-                        </footer>
-                    </header>
-
+                    <MetaBlock
+                            title={data.article.title}
+                            blurb={data.article.blurb}
+                            date={data.article.date}
+                            lastEdit={data.article.last_edit}
+                            wordcount={data.wordcount}
+                            authorLink={data.authorlink}
+                            author={data.article.author}
+                    />
                     <MarkdownBlock content={data.article.content}/>
                     <ArticleEndblock adjacent={data.adjacent}/>
                 </section>
@@ -113,96 +100,6 @@
                 @media (max-width: 768px) {
                     margin: 2vh auto 0 auto;
                     padding: 0 1.4rem;
-                }
-
-                & header {
-                    padding-top: clamp(0.5rem, 1.2vw, 2rem);
-
-                    width: 100%;
-                    display: flex;
-                    flex-direction: column;
-
-                    & h1 {
-                        & span {
-                            padding: 5px 5px 0 5px;
-                            background-color: var(--accent-color);
-                            display: inline;
-                            box-decoration-break: clone;
-
-                            font-size: 2.6rem;
-                            line-height: 2.9rem;
-
-                            font-weight: bolder;
-                            font-family: 'Karla', sans-serif;
-
-                            @media (max-width: 768px) {
-                                font-size: 1.6rem;
-                                line-height: 2.5rem;
-                            }
-                        }
-                    }
-
-                    & .blurb {
-                        & span {
-                            padding: 5px;
-                            background-color: var(--accent-color);
-                            display: inline-block;
-                            font-size: 1.3rem;
-
-                            font-family: 'Anonymous Pro', sans-serif;
-                            font-weight: bolder;
-
-                            @media (max-width: 768px) {
-                                font-size: 1.1rem;
-                            }
-                        }
-                    }
-
-                    & footer {
-                        display: flex;
-                        flex-direction: row;
-                        align-items: center;
-
-                        padding: 5px;
-                        background-color: color-mix(in hsl, var(--accent-color) 93%, black);
-                        font-weight: bolder;
-
-                        @media (max-width: 768px) {
-                            font-size: 1rem;
-                            margin-top: 0.3rem;
-                        }
-
-                        & .databloc {
-                            width: 100%;
-
-                            & time {
-                                font-family: 'Anonymous Pro', sans-serif;
-                            }
-
-                            & address {
-                                display: inline;
-
-                                font-family: 'Anonymous Pro', sans-serif;
-                                font-style: normal;
-
-                                & * {
-                                    display: inline;
-                                    font-family: 'Anonymous Pro', sans-serif;
-                                    font-style: normal;
-                                }
-
-                                & a {
-                                    cursor: pointer;
-
-                                    &:hover {
-                                        text-decoration: underline;
-                                    }
-                                }
-                            }
-                        }
-
-
-                    }
                 }
             }
 
