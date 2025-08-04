ec169b642993061e9a84b5bf1a38db0fc056cac8
Maksiks <maksiksking@gmail.com>
2025-08-04 11:42:24 +0300

    chore: this is going to take a lot of commits to single out, and it doesn't work on dev server consistently too x20`

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,18 +1,7 @@
 <script lang="ts">
-    import {goto} from "$app/navigation";
-
-    const handleTravelToEditor = (e: KeyboardEvent) => {
-        if (e.key === 'ArrowUp') {
-            goto('/admin/dashboard');
-        }
-    }
-
 </script>
 
-<svelte:window on:keydown={handleTravelToEditor}/>
-
-<p>1) First try pressing the up arrow on your keyboard (goto)</p>
-<p>2) Then trying going through a <a href="/admin/dashboard">link</a> or the url bar</p>
+<a href="/admin/dashboard">Link to /admin/dashboard</a>
 
 <style>
 </style>
