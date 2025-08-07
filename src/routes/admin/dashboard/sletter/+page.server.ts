d30fd57deb889b66cefb22ee26ead307e3a49b49
Maksiks <maksiksking@gmail.com>
2025-08-07 13:33:43 +0300

    chore: even more newsletter control progress x2

--- src/routes/admin/dashboard/sletter/+page.server.ts
+++ src/routes/admin/dashboard/sletter/+page.server.ts
@@ -1,5 +1,8 @@
 import {getClient} from "$lib/utils/getSupabaseClient";
-import {error as sverror} from "@sveltejs/kit";
+import {type Actions, error as sverror} from "@sveltejs/kit";
+import { Resend } from 'resend';
+
+export const prerender = false;
 
 export const load = async ({url}) => {
     const title = url.searchParams.get('title');
@@ -12,6 +15,7 @@ export const load = async ({url}) => {
     const {data: slugs, error} = await supabase
         .from('articles')
         .select('slug')
+        .neq('slettered', true)
     ;
 
     if (error) {
@@ -27,3 +31,71 @@ export const load = async ({url}) => {
         slugs
     }
 }
+
+export const actions = {
+    send: async ({ request }) => {
+        const formData = await request.formData();
+        const text = formData.get('text');
+        const associate = formData.get('associate');
+
+        if (!text && !associate) {
+            console.error('oh no')
+            sverror(500, 'oh no')
+        }
+
+        // actual sending part
+        // NOTE: maximum batch size for Resend is 50
+        // + the 100 a day email limit
+        // filter csv to only use the subscribed ones later
+
+        dotenv.config();
+
+        const resend = new Resend();
+
+        const getEmails = async () => {
+            try {
+                const response = await resend.contacts.list({
+                    audienceId: '2a9064d6-92c6-42c5-a9dc-85fd7b8eaf78',
+                });
+
+                return response?.data?.data.map(obj => obj.email);
+            } catch (error) {
+                console.log(error);
+            }
+        }
+
+        const content =
+            `
+        <p>
+        Hey!
+        </p>
+        <p>
+        Just sending this automatic email as confirmation that you signed up to get occasional updates on Lirith. 
+        </p>
+        <img width="460" src="https://ik.imagekit.io/maksiks/moth.png?tr=w-460" alt="lirith logo">
+        <p>
+        Thanks!
+        </p>
+    `
+
+        const emailsBatch = await getEmails();
+
+        const emailsWithInfo = emailsBatch.map((email) => ({
+            from: 'Maksiks <lirith@chaos-abyss.com>',
+            to: [email],
+            subject: 'Lirith updates',
+            html: content
+        }))
+
+        await resend.batch.send(emailsWithInfo);
+
+        await resend.emails.send([
+            {
+                from: 'Maksiks <lirith@chaos-abyss.com>',
+                to: [email],
+                subject: 'Lirith updates',
+                html: content
+            }
+        ]);
+    }
+} satisfies Actions;
--- src/routes/admin/dashboard/sletter/+page.svelte
+++ src/routes/admin/dashboard/sletter/+page.svelte
@@ -1,4 +1,6 @@
-<script>
+<script lang="ts">
+    import {applyAction, enhance} from "$app/forms";
+
     let text = $state();
 
     const { data } = $props();
@@ -19,6 +21,9 @@
     // TODO: HANDLE THE JWT FOR UNSUBSCRIBE
     //     <small style="color: gray">This one's no good for replies, contact me at maksiks.touch@gmail.com instead.</small><br>
     // <small><a style="color: gray" href="https://www.chaos-abyss.com/api/unsubscribe?jwt=${jwt}&lirith=true">unsubscribe</a></small>
+
+    let confirm = $state(0);
+    let sent = $state(false);
 </script>
 
 <main>
@@ -28,23 +33,47 @@
     <p>
         Whatever you write here will be sent to random people. Don't write anything weird.
     </p>
-    <form>
+    <form method="POST" action="/admin/dashboard/sletter/send" use:enhance={({cancel}) => {
+        confirm += 1;
+        if (confirm < 1) cancel();
+
+        return async ({ result, update }) => {
+            if (result) {sent = true}
+
+            await applyAction(result);
+        };
+    }}>
         <textarea name="text" id="text" bind:value={text}>
         {INITIAL}
         </textarea>
         <label>
-            <select name="select" id="select" bind:value={associate}>
+            <select name="associate" id="associate" bind:value={associate}>
                 {#each data.slugs as slug (slug)}
                     <option>{slug.slug}</option>
                 {/each}
             </select>
         </label>
-        <p>Unsubscribe text and "contact me elsewhere" is handled in code-separately (it needs a jwt)</p>
-        <button type="submit">Send</button>
+        <p>Unsubscribe text and "contact me elsewhere" is handled in-code separately (it needs to math the jwt)</p>
+        {#if confirm === 1}
+            <p class="warning">
+            WARNING: THEY WILL SEE IT, ARE YOU ABSOLUTELY SURE?
+            </p>
+        {/if}
+        {#if sent}
+            <p>
+                Sent!
+            </p>
+        {/if}
+        <button type="submit">{confirm === 1 ? 'Yup!' : 'Send'}</button>
     </form>
 </main>
 
 <style>
+    .warning {
+        color: #ff1f1f;
+        font-weight: bold;
+    }
+
     main {
         width: 100vw;
         margin-top: 2rem;
@@ -67,6 +96,7 @@
 
             & button {
                 all: unset;
+                cursor: pointer;
                 margin-top: 1rem;
                 width: 3rem;
                 border-radius: 16px;
