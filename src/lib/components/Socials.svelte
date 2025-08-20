b8ff44f74379635c10a8e51a48bd62c4942e71c6
Maksiks <maksiksking@gmail.com>
2025-08-20 21:57:37 +0300

    chore: small wrong repo revert

--- src/lib/components/Socials.svelte
+++ src/lib/components/Socials.svelte
@@ -47,12 +47,6 @@
             flex-direction: row;
             align-items: center;
 
-            &:hover {
-                filter: blur(0.3px);
-                transition: all 0.1s;
-                transform: scale(1.003);
-            }
-
             & svg {
                 width: 1.2rem;
                 height: 1.2rem;
